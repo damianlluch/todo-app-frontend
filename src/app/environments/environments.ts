f7f412d1633ebd1ecc642b6a5af38c6305a0163c
colaboradores_taktics <damianclluch@gmail.com>
2023-06-14 16:57:01 +0200

    add styles

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000', // Cambia el puerto si tu aplicación de Nest.js se está ejecutando en un puerto diferente
+  apiUrl: 'http://localhost:3000',
 };
