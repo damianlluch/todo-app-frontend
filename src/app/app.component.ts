aca72f6e4731301cb22841c5289b65d4694c6857
colaboradores_taktics <damianclluch@gmail.com>
2023-06-14 09:03:17 +0200

    add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   tasks: Task[] = [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
-    FormsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
